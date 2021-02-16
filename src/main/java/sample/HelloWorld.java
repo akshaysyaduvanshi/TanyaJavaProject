7b39a4c729fa32b5ef487a6808dc52f4df7974d4
Tanya <titoria@paloaltonetworks.com>
2021-02-16 21:57:53 +0530

    adding queue program

--- src/main/java/sample/HelloWorld.java
+++ src/main/java/sample/HelloWorld.java
@@ -2,22 +2,16 @@ import java.util.*;
 class Main {
 
   public static void main(String[] args) {
-    LinkedList<Integer> list=new LinkedList<>();
-    list.add(17);
-    list.add(20);
-    list.add(76);
-    list.add(81);
-    list.add(5);
-    list.add(100);
-    System.out.println("Original list is "+list);
-    System.out.println("Last element is "+list.getLast());
-    System.out.println("First element is "+list.getFirst());
-    System.out.println("Whether list contains 76 "+list.contains(76));
-    System.out.println("Whether list contains 86 "+list.contains(86));
-    list.remove(4);
-    System.out.println("After removing element at 4th position "+list);
-    list.set(3,1000);
-    System.out.println("After replacing 3rd element with 1000 "+list);
-    System.out.println("Size of list "+list.size());
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 10; i < 15; i++)
+          q.add(i);
+        System.out.println("Original queue "+ q);
+        System.out.println("removed element "+q.remove());
+        System.out.println("Updated Queue "+q);
+        System.out.println("head of queue "+q.peek());
+        System.out.println("Size of queue "+q.size());
+      }
+    }
+
   }
 }
